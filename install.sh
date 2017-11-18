f353ac5bccfd88d51224e41f8970d33ec518497e
telemaque <telemaque>
2017-11-18 14:53:37 +0000

    Install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PYTHON="Python-3.6.3"
+
+if [ "$1" == "" ]; then
+    echo "No name"
+    exit 1
+fi
+
+echo "$1" > /etc/hostname
+hostname "$1"
+apt-get update
+apt-get upgrade -y
+apt-get install emacs-nox python3 autossh screen git
+ssh-keygen -f /root/.ssh/id_rsa -N ""
+git config --global user.name "telemaque"
+git config --global user.email "telemaque"
