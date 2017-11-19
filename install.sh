105a3a508ad579f05594d8faf4ef43179b407810
ulysse <ulysse>
2017-11-19 02:52:07 +0100

    Renaming install repository to scripts repository

--- install.sh
+++ install.sh
@@ -15,4 +15,4 @@ apt-get install emacs-nox python3 autossh screen git
 ssh-keygen -f /root/.ssh/id_rsa -N ""
 git config --global user.name "telemaque"
 git config --global user.email "telemaque"
-curl -L "https://raw.githubusercontent.com/ulysse314/install/master/authorized_keys" -o /root/.ssh/authorized_keys
+curl -L "https://raw.githubusercontent.com/ulysse314/scripts/master/authorized_keys" -o /root/.ssh/authorized_keys
