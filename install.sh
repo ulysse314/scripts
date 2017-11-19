11b4505291e7114493a1a591ee03e07c2d137b1a
ulysse <ulysse>
2017-11-19 03:10:24 +0100

    Fixing name

--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-PYTHON="Python-3.6.3"
-
 if [ "$1" == "" ]; then
     echo "No name"
     exit 1
 fi
 
-echo "$1" > /etc/hostname
-hostname "$1"
+NAME="$1"
+echo "${NAME}" > /etc/hostname
+hostname "${NAME}"
 apt-get update
 apt-get upgrade -y
 apt-get install emacs-nox python3 autossh screen git
 ssh-keygen -f /root/.ssh/id_rsa -N ""
-git config --global user.name "telemaque"
-git config --global user.email "telemaque"
+git config --global user.name "${NAME}"
+git config --global user.email "${NAME}"
 curl -L "https://raw.githubusercontent.com/ulysse314/scripts/master/authorized_keys" -o /root/.ssh/authorized_keys
 git clone git@github.com:ulysse314/scripts.git
 git clone git@github.com:ulysse314/boat.git
