6f714d198c93bce8c1c96bd378b071cdaca29d17
moussaillon <moussaillon>
2017-11-27 03:02:40 +0100

    Adding logs for autossh

--- network.sh
+++ network.sh
@@ -15,6 +15,5 @@ while ! ping -w 2 -c 1 192.168.8.1 ; do sleep 1; done
 echo 2 >> /tmp/test
 ip route add default via 192.168.8.1 dev eth1
 echo 3 >> /tmp/test
-AUTOSSH_LOGFILE='/tmp/4g_autossh.log'
-/usr/bin/autossh -M 0 -v -f -N -o ServerAliveInterval=5 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes ${SSH_4G} -b 192.168.8.100 -p "${TUNNEL_PORT}" "${TUNNEL_USER}@${TUNNEL_SERVER}"
+AUTOSSH_LOGLEVEL=7 AUTOSSH_LOGFILE='/tmp/4g_autossh.log' /usr/bin/autossh -M 0 -v -f -N -o ServerAliveInterval=5 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes ${SSH_4G} -b 192.168.8.100 -p "${TUNNEL_PORT}" "${TUNNEL_USER}@${TUNNEL_SERVER}"
 echo done >> /tmp/test
