bc477fa79cfd73044d245aa7d7b281a644decd53
moussaillon <moussaillon>
2018-03-18 18:04:38 +0100

    Better way to update munin_plugin.py

--- update_install.sh
+++ update_install.sh
@@ -9,10 +9,15 @@ cat /etc/crontab | grep -v ULYSSE314 > /tmp/crontab
 cat /tmp/crontab > /etc/crontab
 cat /home/ulysse314/scripts/crontab >> /etc/crontab
 
-if [ -f /etc/munin/plugins/ulysse314.py ]; then
-  rm /etc/munin/plugins/ulysse314.py
+if [ -e /etc/munin/plugins/ulysse314.py ]; then
+  destination=`realpath /etc/munin/plugins/ulysse314.py`
+  if [ "${destination}" != "/home/ulysse314/scripts/linux/munin_plugin.py" ]; then
+    rm /etc/munin/plugins/ulysse314.py
+  fi
 fi
-ln -s /home/ulysse314/scripts/linux/munin_plugin.py /etc/munin/plugins/ulysse314.py
+if [ ! -e /etc/munin/plugins/ulysse314.py ]; then
+  ln -s /home/ulysse314/scripts/linux/munin_plugin.py /etc/munin/plugins/ulysse314.py
+fi
 
 if [ ! -f /etc/udev/rules.d/99-feather-symlink.rules ]; then
   ln -s /home/ulysse314/scripts/linux/udev-rules /etc/udev/rules.d/99-feather-symlink.rules
