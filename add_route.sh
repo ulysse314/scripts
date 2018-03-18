76fa7195e849a1f7736d17865cd779e921ec9ee6
moussaillon <moussaillon>
2018-03-18 16:06:38 +0100

    Don't wait for the 4G before to start the main script

---
+++ add_route.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DEBUG_FILE=/tmp/route
+echo "waiting for eth1" > $"{DEBUG_FILE}"
+while ! ifconfig eth1 ; do sleep 2; done
+echo "waiting for 192.168.8.1" >> "${DEBUG_FILE}"
+while ! ping -w 2 -c 1 192.168.8.1 ; do sleep 2; done
+ip route add default via 192.168.8.1 dev eth1 2>> "${DEBUG_FILE}"
+echo "route added" >> "${DEBUG_FILE}"
+date >> "${DEBUG_FILE}"
--- network.sh
+++ network.sh
@@ -11,12 +11,8 @@ SSH_4G="-R *:${SSH_4G_PORT}:127.0.0.1:22"
 AUTOSSH_LOGLEVEL=7 AUTOSSH_LOGFILE='/tmp/wifi_autossh.log' /usr/bin/autossh -M 0 -v -f -N -o ServerAliveInterval=5 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes ${SSH_WIFI} -p "${TUNNEL_PORT}" "${TUNNEL_USER}@${TUNNEL_SERVER}"
 echo "ok" > "${DEBUG_FILE}"
 lsusb >> "${DEBUG_FILE}"
-while ! ifconfig eth1 ; do sleep 1; done
-echo "1" >> "${DEBUG_FILE}"
-while ! ping -w 2 -c 1 192.168.8.1 ; do sleep 1; done
-echo 2 >> "${DEBUG_FILE}"
-ip route add default via 192.168.8.1 dev eth1
-echo 3 >> "${DEBUG_FILE}"
+/home/ulysse314/scripts/add_route.sh &
 AUTOSSH_LOGLEVEL=7 AUTOSSH_LOGFILE='/tmp/4g_autossh.log' /usr/bin/autossh -M 0 -v -f -N -o ServerAliveInterval=5 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes ${SSH_4G} -b 192.168.8.100 -p "${TUNNEL_PORT}" "${TUNNEL_USER}@${TUNNEL_SERVER}"
+date >> "${DEBUG_FILE}"
 echo done >> "${DEBUG_FILE}"
 /home/ulysse314/boat/start.sh boat "${BOAT_NAME}"
