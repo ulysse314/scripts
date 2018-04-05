7f85f42558c63904f6f735d5366269df6a1bb1a5
moussaillon <moussaillon>
2018-04-05 02:13:07 +0200

    Using the one build directory per project to find the binary

--- arduino/upload.sh
+++ arduino/upload.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+# upload.sh [file_to_upload] [port]
 
-port="$1"
+file_to_upload="$1"
+port="$2"
 
-/home/ulysse314/arduino/arduino15/packages/arduino/tools/bossac/1.7.0/bossac -i -d "--port=${port}" -U true -i -e -w -v /tmp/arduino_build/arduino.ino.bin -R
+if [[ "${file_to_upload}" = "" ]]; then
+  file_to_upload="/tmp/arduino_build/arduino.ino/arduino.ino.bin"
+fi
+if [[ "${port}" = "" ]]; then
+  port=`/home/ulysse314/scripts/arduino/serial_ports.sh "239a_000b"`
+fi
+
+/home/ulysse314/arduino/arduino15/packages/arduino/tools/bossac/1.7.0/bossac -i -d "--port=${port}" -U true -i -e -w -v "${file_to_upload}" -R
 exit $?
