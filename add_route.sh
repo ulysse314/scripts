2874d5cce615d072ca25091bd2d303e57e545c55
moussaillon <moussaillon>
2018-04-07 12:50:03 +0200

    Fixing log for 4g router script

--- add_route.sh
+++ add_route.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DEBUG_FILE=/tmp/route
-echo "waiting for eth1" > $"{DEBUG_FILE}"
+echo "waiting for eth1" > "${DEBUG_FILE}"
 while ! ifconfig eth1 ; do sleep 2; done
 echo "waiting for 192.168.8.1" >> "${DEBUG_FILE}"
 while ! ping -w 2 -c 1 192.168.8.1 ; do sleep 2; done
