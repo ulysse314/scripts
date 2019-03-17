330bf261339d41aec021ec99d9ad3ed498a66bea
moussaillon <moussaillon>
2019-03-17 13:55:54 +0100

    Monitor the default route and change it as soon as the 4G is back

--- add_route.sh
+++ add_route.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
 DEBUG_FILE=/tmp/route
-echo "waiting for eth1" > "${DEBUG_FILE}"
-while ! ifconfig eth1 ; do sleep 2; done
-echo "waiting for 192.168.8.1" >> "${DEBUG_FILE}"
-while ! ping -w 2 -c 1 192.168.8.1 ; do sleep 2; done
-ip route add default via 192.168.8.1 dev eth1 2>> "${DEBUG_FILE}"
-echo "route added" >> "${DEBUG_FILE}"
-date >> "${DEBUG_FILE}"
+INTERFACE="eth1"
+PING_IP="8.8.8.8"
+
+echo "waiting for ${INTERFACE}" > "${DEBUG_FILE}"
+while [[ ! `ifconfig "${INTERFACE}"` ]]; do
+  sleep 2
+done
+
+echo "Start monitoring" > "${DEBUG_FILE}"
+while [[ true ]]; do
+  ping -I "${INTERFACE}" -c 1 "${PING_IP}"
+  if [[ "$?" == "1" ]]; then
+    DEFAULT_INTERFACE=`ip -o route get "${PING_IP}" | perl -nle 'if ( /dev\s+(\S+)/ ) {print $1}'`
+    if [[ "${DEFAULT_INTERFACE}" == "${INTERFACE}" ]]; then
+      ip route del default via 192.168.8.1 dev "${INTERFACE}" 2>> "${DEBUG_FILE}"
+      echo "Remove default route to ${INTERFACE}" > "${DEBUG_FILE}"
+    fi
+  else
+    DEFAULT_INTERFACE=`ip -o route get "${PING_IP}" | perl -nle 'if ( /dev\s+(\S+)/ ) {print $1}'`
+    if [[ "${DEFAULT_INTERFACE}" == "${INTERFACE}" ]]; then
+      ip route add default via 192.168.8.1 dev "${INTERFACE}" 2>> "${DEBUG_FILE}"
+      echo "Add default route to ${INTERFACE}" > "${DEBUG_FILE}"
+    fi
+  fi
+  sleep 5
+done
