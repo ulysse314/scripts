eb0610b4362b108a72ca7d1da7e520ba100c7b61
moussaillon <moussaillon>
2019-03-17 10:55:08 +0100

    The camera should be started before the boat script (the script never returns)

--- boot.sh
+++ boot.sh
@@ -25,11 +25,12 @@ lsusb >> "${DEBUG_FILE}"
 /home/ulysse314/scripts/add_route.sh &
 #AUTOSSH_LOGLEVEL=7 AUTOSSH_LOGFILE='/tmp/4g_autossh.log' /usr/bin/autossh -M 0 -v -f -N -o ServerAliveInterval=5 -o ServerAliveCountMax=3 -o ExitOnForwardFailure=yes ${SSH_4G} ${CAM_4G} -b 192.168.8.100 -p "${TUNNEL_PORT}" "${TUNNEL_USER}@${TUNNEL_SERVER}"
 date >> "${DEBUG_FILE}"
-echo done >> "${DEBUG_FILE}"
-/home/ulysse314/boat/start.sh boat "${BOAT_NAME}"
 
 if [[ "${CAMERA_ID}" == "PI" ]]; then
   uv4l --driver raspicam --server-option --port=8081 --auto-video_nr --width 640 --height 480 --encoding jpeg -–framerate 30 1&>> "${DEBUG_FILE}"
 elif [[ "${CAMERA_ID}" != "" ]]; then
   uv4l --driver uvc --syslog-host localhost --device-id "${CAMERA_ID}" --server-option --port=8081 --auto-video_nr 1&>> "${DEBUG_FILE}"
 fi
+
+echo done >> "${DEBUG_FILE}"
+/home/ulysse314/boat/start.sh boat "${BOAT_NAME}"
