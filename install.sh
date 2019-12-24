fddd01342a558de6e8d47b223fc2a7891106ac92
moussaillon <moussaillon>
2019-12-24 01:58:02 +0000

    Load ulyssse script if it already exists

--- install.sh
+++ install.sh
@@ -50,6 +50,8 @@ if [ ! -f /etc/ulysse314/script ]; then
     echo "Needs /etc/ulysse314/script"
     exit 1
   fi
+else
+  source /etc/ulysse314/script
 fi
 if [ ! -f /etc/ulysse314/name ]; then
   echo "${BOAT_NAME}" > /etc/ulysse314/name
