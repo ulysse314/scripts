211b1f6588154930f5655d256881204558c74fb2
moussaillon <moussaillon>
2019-12-27 22:45:50 +0100

    Better way to get a port, based in ID_MODEL or ID_SERIAL

--- arduino/serial_ports.sh
+++ arduino/serial_ports.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-description="$1"
+key="$1"
+description="$2"
 
 for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
   syspath="${sysdevpath%/dev}"
-  devname="$(udevadm info -q name -p $syspath)"
-  [[ "$devname" == "bus/"* ]] && continue
-  eval "$(udevadm info -q property --export -p $syspath)"
-  [[ -z "$ID_SERIAL" ]] && continue
-  [[ "${ID_SERIAL}" != "${description}" ]] && continue
-  echo "${devname}"
-  exit 0
+  devname="$(udevadm info -q name -p ${syspath})"
+  [[ "${devname}" == "bus/"* ]] && continue
+  eval "$(udevadm info -q property --export -p ${syspath})"
+  if [[ "${key}" == "ID_MODEL" ]]; then
+    [[ -z "${ID_MODEL}" ]] && continue
+    [[ "${ID_MODEL}" != "${description}" ]] && continue
+    echo "${devname}"
+    exit 0
+  elif [[ "${key}" == "ID_SERIAL" ]]; then
+    [[ -z "${ID_SERIAL}" ]] && continue
+    [[ "${ID_SERIAL}" != "${description}" ]] && continue
+    echo "${devname}"
+    exit 0
+  fi
 done
 exit 1
