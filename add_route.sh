218a8fe004b7c81c2608ea5340681d69ebaeb7a4
moussaillon <moussaillon>
2020-07-16 19:50:24 +0200

    add_route.sh more flexible to choose the primary interface

--- add_route.sh
+++ add_route.sh
@@ -8,25 +8,32 @@ DEBUG_FILE=/tmp/route
 G4_INTERFACE="eth1"
 WIFI_INTERFACE="wlan0"
 TEST_IP="8.8.8.8"
+PRIMARY_INTERFACE="${G4_INTERFACE}"
+SECONDARY_INTERFACE="${WIFI_INTERFACE}"
 
 while [[ ! `ifconfig "${G4_INTERFACE}"` ]]; do sleep 2; done
 
 echo "Start monitoring with default interface ${DEFAULT_INTERFACE}" > "${DEBUG_FILE}"
 while [[ true ]]; do
-  ifconfig "${G4_INTERFACE}" > /dev/null
-  IS_G4_INTERFACE_VALID="$?"
-  if [[ "${IS_G4_INTERFACE_VALID}" == "0" ]]; then
-    DEFAULT_INTERFACE=`ip -o route get "${TEST_IP}" | perl -nle 'if ( /dev\s+(\S+)/ ) {print $1}'`
-    ping -I "${G4_INTERFACE}" -c 1 -W 5 "${TEST_IP}"
-    if [[ "$?" == "1" && "${DEFAULT_INTERFACE}" == "${G4_INTERFACE}" ]]; then
-      echo "Decrease 4G"
-      ifmetric "${G4_INTERFACE}" 201
-      ifmetric "${WIFI_INTERFACE}" 200
-    elif [[ "$?" == "0" && "${DEFAULT_INTERFACE}" == "${WIFI_INTERFACE}" ]]; then
-      echo "Increase 4G"
-      ifmetric "${G4_INTERFACE}" 200
-      ifmetric "${WIFI_INTERFACE}" 201
-    fi
-  fi
   sleep 5
+  ifconfig "${PRIMARY_INTERFACE}" > /dev/null
+  if [[ "$?" != "0" ]]; then
+    continue
+  fi
+  ifconfig "${SECONDARY_INTERFACE}" > /dev/null
+  if [[ "$?" != "0" ]]; then
+    continue
+  fi
+  DEFAULT_INTERFACE=`ip -o route get "${TEST_IP}" | perl -nle 'if ( /dev\s+(\S+)/ ) {print $1}'`
+  ping -I "${PRIMARY_INTERFACE}" -c 1 -W 5 "${TEST_IP}"
+  IS_PING_VALID="$?"
+  if [[ "${IS_PING_VALID}" != "0" && "${DEFAULT_INTERFACE}" == "${PRIMARY_INTERFACE}" ]]; then
+    echo "Primary interface fails, and is the default, needs to be lower priority"
+    ifmetric "${PRIMARY_INTERFACE}" 201
+    ifmetric "${SECONDARY_INTERFACE}" 200
+  elif [[ "${IS_PING_VALID}" == "0" && "${DEFAULT_INTERFACE}" == "${SECONDARY_INTERFACE}" ]]; then
+    echo "Primary interface works, and is not the default, needs to be higher priority"
+    ifmetric "${PRIMARY_INTERFACE}" 200
+    ifmetric "${SECONDARY_INTERFACE}" 201
+  fi
 done
