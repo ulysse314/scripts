8b874cc2634316b66e84f00da161982243b14e61
moussaillon <moussaillon>
2021-11-24 23:10:17 +0000

    Replace --build-properties to --build-property in compile.sh

--- arduino/compile.sh
+++ arduino/compile.sh
@@ -9,7 +9,7 @@ if [[ "${INO_FULL_PATH}" = "" ]]; then
   INO_FULL_PATH="${MAIN_DIR}/boat/arduino/arduino.ino"
 fi
 
-"${MAIN_DIR}/arduino/arduino-cli" --config-file "${ARDUINO_CLI_CONFIG}" compile "${INO_FULL_PATH}" --libraries "${ARDUINO_DIR}/libraries" --fqbn "${MAIN_BOARD_FQBN_PACKAGER}:${MAIN_BOARD_FQBN_ARCHITECTURE}:${MAIN_BOARD_FQBN_BOARD}" --build-properties "compiler.cpp.extra_flags=-DBOAT_ID=${BOAT_ID}" --warnings all
+"${MAIN_DIR}/arduino/arduino-cli" --config-file "${ARDUINO_CLI_CONFIG}" compile "${INO_FULL_PATH}" --libraries "${ARDUINO_DIR}/libraries" --fqbn "${MAIN_BOARD_FQBN_PACKAGER}:${MAIN_BOARD_FQBN_ARCHITECTURE}:${MAIN_BOARD_FQBN_BOARD}" --build-property "compiler.cpp.extra_flags=-DBOAT_ID=${BOAT_ID}" --warnings all
 
 RESULT=`echo $?`
 if [[ "${RESULT}" == "0" ]]; then
